ab6e21ef5bd0b8a93d6b37544e751692231ac75e
EIto-Ito <127803688+EIto-Ito@users.noreply.github.com>
2023-10-11 20:33:18 +0900

    aを出力

--- api/index.py
+++ api/index.py
@@ -1,10 +1,26 @@
 from http.server import BaseHTTPRequestHandler
+import json
+from urllib.parse import parse_qs, urlparse
+
 
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
+        print(self.date_time_string())
+
+        # リクエストURIを解析
+        parsed_url = urlparse(self.path)
+
+        # クエリストリングを解析
+        query_parameters = parse_qs(parsed_url.query)
+        print(query_parameters)
+        # クエリストリングから特定のパラメータを取得
+        if "q" in query_parameters:
+            param_value = query_parameters["q"][0]
+        print(param_value)
+
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header("Content-type", "application/json")
+
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write("a".encode("utf-8"))
         return
